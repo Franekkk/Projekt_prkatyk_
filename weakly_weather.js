c04ac86e719de2c5fc9dc0a945411e656c91c0d0
Franciszek <kamieniecki.franek@gmail.com>
2024-02-15 14:36:55 +0100

    Aktualizacja praktyk: 15.02.2024_komentarz

--- weakly_weather.js
+++ weakly_weather.js
@@ -110,22 +110,22 @@
 // console.log(isSorted2([1,2,3,4,5]));
 // console.log(isSorted2([5,4,3,2,25]));
 
-let timesTable  = [
-    [1,2,3,4,5],
-    [2,4,6,8,10],
-    [3,6,9,12,15],
-    [4,8,12,16,20],
-    [5,10,15,20,25],
-]
+// let timesTable  = [
+//     [1,2,3,4,5],
+//     [2,4,6,8,10],
+//     [3,6,9,12,15],
+//     [4,8,12,16,20],
+//     [5,10,15,20,25],
+// ]
 
-for (let row = 0; row < timesTable.length; row++){
-    let line = ""
-    for(let col = 0; col < timesTable[row].length; col++) {
-        let value = timesTable[row][col]
-        line += value + " "
-    }
-    console.log(line);
-}
+// for (let row = 0; row < timesTable.length; row++){
+//     let line = ""
+//     for(let col = 0; col < timesTable[row].length; col++) {
+//         let value = timesTable[row][col]
+//         line += value + " "
+//     }
+//     console.log(line);
+// }
 
 
 
