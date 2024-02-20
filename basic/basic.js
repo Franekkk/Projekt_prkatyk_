0fc03dd84057cde0dfd2cea7b88f4f2c7879897b
franek <kamieniecki.franek@gmail.com>
2024-02-20 14:58:11 +0100

    Aktualizacja praktyk: 20.02.2024_poprawu_stylu_&_dziennik

--- basic/basic.js
+++ basic/basic.js
@@ -1,30 +1,26 @@
-const addBtn = document.querySelector('.przycisk')
-const list = document.querySelector('myList')
-const field = document.getElementById("input1")
-let arrayValue = []
+const addBtn = document.getElementById("subBtn");
+const list = document.querySelector("myList");
+const field = document.getElementById("input1");
 
+let arrayValue = [];
 
-function wpis(){
-  if(field.value === '' ){
-    alert('pole musi byc wypelnione')
-    field.value = ''
-  }else{
-   arrayValue.push(field.value)
-    li2 = document.createElement('li')
-    li2.innerHTML = arrayValue
-    document.getElementById('MojaLista').appendChild(li2) 
-    field.value = ''
-    arrayValue.pop(field.value)
-    }
+function entry() {
+  if (field.value === "") {
+    alert("pole musi byc wypelnione");
+    field.value = "";
+  } else {
+    arrayValue.push(field.value);
+    li2 = document.createElement("li");
+    li2.innerHTML = arrayValue;
+    document.getElementById("ListIdenti").appendChild(li2);
+    field.value = "";
+    arrayValue.pop(field.value);
   }
+}
 
-
-
-
-
-addBtn.addEventListener('click' ,wpis)
-field.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter'){
-    wpis()
+addBtn.addEventListener("click", entry);
+field.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    entry();
   }
-})
+});
