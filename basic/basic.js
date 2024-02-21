c41d43c8aee727b8ab8434934c72b07b738aa4e8
franek <kamieniecki.franek@gmail.com>
2024-02-21 10:42:05 +0100

    21.02.2024_basic_przycisk_edit

--- basic/basic.js
+++ basic/basic.js
@@ -1,8 +1,13 @@
 const addBtn = document.getElementById("subBtn");
 const list = document.querySelector("myList");
 const field = document.getElementById("input1");
+const EditBtn = document.querySelector(".FIL");
+const field2 = document.querySelector(".input2");
 
 let arrayValue = [];
+let enters = [];
+let edited;
+const olElement = document.getElementById("ListIdenti");
 
 function entry() {
   if (field.value === "") {
@@ -10,11 +15,36 @@ function entry() {
     field.value = "";
   } else {
     arrayValue.push(field.value);
+    enters.push(field.value);
     li2 = document.createElement("li");
     li2.innerHTML = arrayValue;
     document.getElementById("ListIdenti").appendChild(li2);
     field.value = "";
     arrayValue.pop(field.value);
+  }
+}
+
+function edit() {
+  if (field2.value === "") {
+    EditBtn.disabled = true;
+  } else {
+    EditBtn.disabled = false;
+    let edited = enters.filter((enter) => enter === field2.value);
+    console.log(edited);
+    while (arrayValue.length !== 0) {
+      arrayValue.pop();
+    }
+    console.log(arrayValue);
+    if (arrayValue.length === 0) {
+      while (olElement.firstChild) {
+        olElement.removeChild(olElement.firstChild);
+      }
+      for (let i = 0; i < edited.length; i++) {
+        li2 = document.createElement("li");
+        li2.innerHTML = edited[i];
+        document.getElementById("ListIdenti").appendChild(li2);
+      }
+    }
   }
 }
 
@@ -24,3 +54,4 @@ field.addEventListener("keypress", function (e) {
     entry();
   }
 });
+EditBtn.addEventListener("click", edit);
