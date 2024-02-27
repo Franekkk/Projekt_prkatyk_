1867c4a3ce9d24d767db36e3ef7b1d02c3acdc2c
Franekkk <kamieniecki.franek@gmail.com>
2024-02-27 14:55:04 +0100

    destukturyzacja_optymalizacja

--- basic/basic.js
+++ basic/basic.js
@@ -4,15 +4,17 @@ const field = document.getElementById("input1");
 const field2 = document.getElementById("input2");
 const field3 = document.getElementById("input3");
 const divList = document.getElementById("divList");
-const editBtn = document.querySelector(".filter-btn");
+const filtrBtn = document.querySelector(".filter-btn");
 const filter_input = document.querySelector(".input2");
 
+const [CHANGE_COLOR, ENTER_INPUTS, CELL] = ["changeColor", "enterInputs", "cell"];
+const [CLICK, KEYPRESS, ENTER] = ["click", "keypress", "Enter"];
+const [LI, DIV] = ["li", "div"];
+const EMPTY_STRING = "";
 const inputArr = [];
-
 let enterValues = [];
-let edited;
 const fields = [field, field2, field3];
-const checkEmpty = (element) => element.value === "";
+const checkEmpty = (element) => element.value === EMPTY_STRING;
 
 function entry() {
   if (fields.some(checkEmpty)) {
@@ -26,60 +28,60 @@ function entry() {
     fields.forEach(resetField);
 
     enterValues.push(field.value);
-    li2 = document.createElement("li");
+    li2 = document.createElement(LI);
     divList.appendChild(li2);
     inputArr.push(combinedObj);
 
-    const mappedInputs = inputArr.map(mapDivs);
-    divList.innerHTML = mappedInputs.join("");
-    ``;
+    divList.innerHTML = inputArr.map(mapDivs).join(EMPTY_STRING);
   }
 }
 
 function mapDivs(inputObj) {
   const { verb, sp, pp } = inputObj;
-  return [verb, sp, pp].map(prepairDiv).join("");
+  return [verb, sp, pp].map(prepairDiv).join(EMPTY_STRING);
 }
 
 function resetField(field) {
-  field.value = "";
-  field.classList.remove("changeColor");
+  field.value = EMPTY_STRING;
+  field.classList.remove(CHANGE_COLOR);
 }
 
 function prepairDiv(inputValue) {
-  const newDiv = document.createElement("div");
+  const newDiv = document.createElement(DIV);
   newDiv.textContent = inputValue;
-  newDiv.classList.add("gridBtn");
+  newDiv.classList.add(CELL);
   return newDiv.outerHTML;
 }
 function chooseClass(element) {
-  element.className = element.value !== "" ? "enterInputs" : "changeColor";
+  element.className = element.value !== EMPTY_STRING ? ENTER_INPUTS : CHANGE_COLOR;
 }
-function edit() {
-  if (filter_input.value === "") {
-    editBtn.disabled = true;
+function createLi(element) {
+  const newLi = document.createElement(LI);
+  newLi.innerHTML = element;
+  return newLi.outerHTML;
+}
+function filtrInputs() {
+  if (filter_input.value === EMPTY_STRING) {
+    filtrBtn.disabled = true;
   } else {
-    editBtn.disabled = false;
-    const filtered = enterValues.filter(
-      (enterValue) => enterValue === filter_input.value
-    );
-    const createArr = filtered.map((item) => {
-      const newLi = document.createElement("li");
-      newLi.innerHTML = item;
-      return newLi.outerHTML;
-    });
+    filtrBtn.disabled = false;
 
-    divList.innerHTML = createArr.join("");
+    const createArr = enterValues
+      .filter((enterValue) => enterValue === filter_input.value)
+      .map(createLi)
+      .join(EMPTY_STRING);
+
+    divList.innerHTML = createArr;
   }
 }
 
-addBtn.addEventListener("click", entry);
+addBtn.addEventListener(CLICK, entry);
 fields.forEach((field) => {
-  field.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
+  field.addEventListener(KEYPRESS, function (event) {
+    if (event.key === ENTER) {
       entry();
     }
   });
 });
 
-editBtn.addEventListener("click", edit);
+filtrBtn.addEventListener(CLICK, filtrInputs);
